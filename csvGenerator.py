3d754bc7028db0eaad3657e339deb44449e5cdd6
jimmyglasscock <jamglass42@gmail.com>
2019-11-13 12:47:02 -0700

    now automatically creates csvs for all files in dir

--- csvGenerator.py
+++ csvGenerator.py
@@ -1,4 +1,6 @@
 import sys
+import os
+from os import walk
 
 def promptUser():
 	print("----- CSV Generator -----")
@@ -7,33 +9,39 @@ def promptUser():
 	return text
 
 def echoCommaSeperated(path):
-	f = open(path, "r")
-	text = f.read()
-	text = text.splitlines()
-	csvData = ""
-	for line in text:
-		if "CLASS " in line and "SUB CLASS" not in line:
-			classNum = line[6:9]
+	if(path != '.DS_Store'):
+		f = open(path, "r")
+		text = f.read()
+		text = text.splitlines()
+		csvData = ""
+		for line in text:
+			if "CLASS " in line and "SUB CLASS" not in line:
+				classNum = line[6:9]
 
-			BI = line[43:52]
-			lastSpace = BI.rfind(' ')
-			BI = BI[lastSpace+1:52]
-			if(line[52:53] == '-'):
-				BI = '-'+BI
-			
-			EI = line[113:121]
-			lastSpace = EI.rfind(' ')
-			EI = EI[lastSpace+1:121]
-			if(line[121:122] == '-'):
-				EI = '-'+EI
-
-
-			csvData += classNum+","+BI+","+EI+"\n"
-
-	csv = open(path[0:-4]+".csv", "w")
-	csv.write(csvData)
-	csv.close()
+				BI = line[43:52]
+				lastSpace = BI.rfind(' ')
+				BI = BI[lastSpace+1:52]
+				if(line[52:53] == '-'):
+					BI = '-'+BI
+				
+				EI = line[113:121]
+				lastSpace = EI.rfind(' ')
+				EI = EI[lastSpace+1:121]
+				if(line[121:122] == '-'):
+					EI = '-'+EI
 
 
-path = promptUser()
-echoCommaSeperated(path)
+				csvData += classNum+","+BI+","+EI+"\n"
+
+		csv = open(path[0:-4]+".csv", "w")
+		csv.write(csvData)
+		csv.close()
+
+
+#path = promptUser()
+mypath = os.path.dirname(os.path.realpath(__file__))
+f = []
+for(dirpath, dirnames, filenames) in walk(mypath):
+	for file in filenames:
+		echoCommaSeperated(file)
+
